2e78739e4298a30a393e971a03f55cbdc312291a
benxiong.hu <33918700+1301536601@users.noreply.github.com>
2021-06-04 22:50:03 +0800

    新增对应原子性操作

---
+++ MultiThreadingDemo/Demo/InterlockedDemo.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading;
+
+namespace MultiThreadingDemo.Demo
+{
+    public static class InterlockedDemo
+    {
+        public static long IncrementNum = 0;
+
+        /// <summary>
+        /// 自增
+        /// </summary>
+        public static void Increment()
+        {
+            Interlocked.Increment(ref IncrementNum);
+        }
+
+        /// <summary>
+        /// 自减
+        /// </summary>
+        public static void Decrement()
+        {
+            Interlocked.Decrement(ref IncrementNum);
+        }
+
+        /// <summary>
+        /// IncrementNum=IncrementNum+number
+        /// </summary>
+        /// <param name="number"></param>
+        public static void Add(int number)
+        {
+            Interlocked.Add(ref IncrementNum, number);
+        }
+
+        /// <summary>
+        /// 取值时先取 IncrementNum的值 在下次取值时得到的值为上一次的 IncrementNum+number
+        /// </summary>
+        /// <param name="number"></param>
+        public static long Exchange(int number)
+        {
+            var oldValue = Interlocked.Exchange(ref IncrementNum, number);
+            return oldValue;
+        }
+
+        /// <summary>
+        /// 先对比当前的contrastNumber和当前的 IncrementNum 如果相等 则 IncrementNum等于number；
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="contrastNumber"></param>
+        public static void CompareExchange(int number, int contrastNumber)
+        {
+            Interlocked.CompareExchange(ref IncrementNum, number, contrastNumber);
+        }
+
+        /// <summary>
+        /// 读取值
+        /// </summary>
+        public static long Read()
+        {
+            var number = Interlocked.Read(ref IncrementNum);
+            return number;
+        }
+    }
+}
--- MultiThreadingDemo/Program.cs
+++ MultiThreadingDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 
 using MultiThreadingDemo.Demo;
@@ -11,6 +12,10 @@ namespace MultiThreadingDemo
     /// </summary>
     class Program
     {
+        public static LocalDataStoreSlot Slot;
+
+        public static ThreadLocal<string> Local;
+
         static void Main(string[] args)
         {
 
@@ -29,24 +34,70 @@ namespace MultiThreadingDemo
 
             #endregion
 
-
             #region LocalDataStoreSlotDemo
 
-            var slot = Thread.AllocateNamedDataSlot("slot");
-            Thread.SetData(slot, "localDataStoreSlotDemo");
+            //var slot = Thread.AllocateNamedDataSlot("slot");
+            //Thread.SetData(slot, "localDataStoreSlotDemo");
 
-            var th = new Thread(() =>
-            {
-                Thread.SetData(slot, "DiDi"); // 假设新开线程不设置ThreadStaticNum 则slot为默认值
-                ShowLocalDataStoreSlot();
-            });
-            th.Start();
-            th.Join();
+            //var th = new Thread(() =>
+            //{
+            //    Thread.SetData(slot, "DiDi"); // 假设新开线程不设置ThreadStaticNum 则slot为默认值
+            //    ShowLocalDataStoreSlot();
+            //});
+            //th.Start();
+            //th.Join();
 
-            Console.WriteLine($@"MainThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Thread.GetData(slot)}");
+            //Console.WriteLine($@"MainThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Thread.GetData(slot)}");
 
 
-            Thread.FreeNamedDataSlot("localDataStoreSlotDemo");
+            //Thread.FreeNamedDataSlot("localDataStoreSlotDemo");
+            #endregion
+
+            #region 未命名LocalDataStoreSlot
+
+            //Slot = Thread.AllocateDataSlot();
+
+            ////设置TLS中的值
+            //Thread.SetData(Slot, "hehe");
+
+            ////修改TLS的线程
+            //var th = new Thread(() =>
+            //{
+            //    Thread.SetData(Slot, "Mgen");
+            //    UnNamedShowLocalDataStoreSlot();
+            //});
+
+            //th.Start();
+            //th.Join();
+            //Console.WriteLine($@"MainThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Thread.GetData(Slot)}");
+
+            #endregion
+
+            #region ThreadLocal
+
+            //Local = new ThreadLocal<string>(() => "hehe");
+            //var th = new Thread(() =>
+            //{
+            //    Local.Value = "Mgen";
+            //    ShowThreadLocal();
+            //});
+
+            //th.Start();
+            //th.Join();
+            //Console.WriteLine($@"MainThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Local.Value}");
+
+            #endregion
+
+            #region Interlocked
+
+            var oldValue = InterlockedDemo.Exchange(10);
+            Console.WriteLine($@"*******oldValue={oldValue}**********");
+            InterlockedDemo.Add(10);
+            InterlockedDemo.Increment();
+            InterlockedDemo.Decrement();
+            InterlockedDemo.CompareExchange(30, 20);
+            Console.WriteLine(InterlockedDemo.Read());
+
             #endregion
 
             Console.ReadKey();
@@ -56,6 +107,16 @@ namespace MultiThreadingDemo
         {
             LocalDataStoreSlot dataSlot = Thread.GetNamedDataSlot("slot");
             Console.WriteLine($@"Current Is New Thread ,ThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Thread.GetData(dataSlot)}");
+        }
+
+        public static void UnNamedShowLocalDataStoreSlot()
+        {
+            Console.WriteLine($@"Current Is New Thread ,ThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Thread.GetData(Slot)}");
+        }
+
+        public static void ShowThreadLocal()
+        {
+            Console.WriteLine($@"Current Is New Thread ,ThreadId Is {Thread.CurrentThread.ManagedThreadId} Current Result Is {Local.Value}");
         }
     }
 }
